481be15ab9f9eafb462071a3370eb28885be5c22
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-01-29 15:13:46 -0300

    code optimization and clean code

--- main.py
+++ main.py
@@ -4,68 +4,35 @@ from mining import *
 def search(info, tipo, data, numero=10):
 
     if(tipo == "data"):
-        if(info in data):
-            lista = data[info]
-            i = 0
-            for acidente in lista:
-                print("data: {}\nDescrição: {}\nVeiculo: {}\nComplemento: {}\nOcorrencia: {}\nBairro: {}\nHora:{}\nendereco: {}\nMaps:{}\n"
-                       .format(acidente["data"], acidente["descricao"], acidente["veiculo"], acidente["complemento"], acidente["ocorrencia"], acidente["bairro"], 
-                       acidente["hora"], acidente["endereco"], acidente["link"]))
-
-                i+=1
-                if(i == numero):
-                    break
-
-        else:
+        if(info not in data):
             print("Data inexistente")
+            return
             
     elif(tipo == "bairro"):
-        if(info in data):
-            lista = data[info]
-            i = 0
-            for acidente in lista:
-                print("data: {}\nDescrição: {}\nVeiculo: {}\nComplemento: {}\nOcorrencia: {}\nBairro: {}\nHora:{}\nendereco: {}\nMaps:{}\n"
-                       .format(acidente["data"], acidente["descricao"], acidente["veiculo"], acidente["complemento"], acidente["ocorrencia"], acidente["bairro"], 
-                       acidente["hora"], acidente["endereco"], acidente["link"]))
-
-                i+=1
-                if(i == numero):
-                    break
-
-        else:
+        if(info not in data):
             print("Bairro inexistente")
+            return
 
     elif(tipo == "veiculo"):
-        if(info in data):
-            lista = data[info]
-            i = 0
-            for acidente in lista:
-                print("data: {}\nDescrição: {}\nVeiculo: {}\nComplemento: {}\nOcorrencia: {}\nBairro: {}\nHora:{}\nendereco: {}\nMaps:{}\n"
-                       .format(acidente["data"], acidente["descricao"], acidente["veiculo"], acidente["complemento"], acidente["ocorrencia"], acidente["bairro"], 
-                       acidente["hora"], acidente["endereco"], acidente["link"]))
-
-                i+=1
-                if(i == numero):
-                    break
-
-        else:
+        if(info not in data):
             print("Veiculo inexistente")
+            return
     
     elif(tipo == "feridos"):
-        if(info in data):
-            lista = data[info]
-            i = 0
-            for acidente in lista:
-                print("data: {}\nDescrição: {}\nVeiculo: {}\nComplemento: {}\nOcorrencia: {}\nBairro: {}\nHora:{}\nendereco: {}\nMaps:{}\n"
-                       .format(acidente["data"], acidente["descricao"], acidente["veiculo"], acidente["complemento"], acidente["ocorrencia"], acidente["bairro"], 
-                       acidente["hora"], acidente["endereco"], acidente["link"]))
-
-                i+=1
-                if(i == numero):
-                    break
-                
-        else:
+        if(info not in data):
             print("Quantidade de feridos inexistente")
+            return
+
+    lista = data[info]
+    i = 0
+    for acidente in lista:
+        print("data: {}\nDescrição: {}\nVeiculo: {}\nComplemento: {}\nOcorrencia: {}\nBairro: {}\nHora:{}\nendereco: {}\nMaps:{}\n"
+                .format(acidente["data"], acidente["descricao"], acidente["veiculo"], acidente["complemento"], acidente["ocorrencia"], acidente["bairro"], 
+                acidente["hora"], acidente["endereco"], acidente["link"]))
+
+        i+=1
+        if(i == numero):
+            break
 
 
 def menu(dados):
@@ -73,7 +40,11 @@ def menu(dados):
     bairro, data, feridos, veiculo = get_data(dados)
 
     while True:
-        pesquisa = input("Pesquisar por: ")
+        try:
+            pesquisa = input("Pesquisar por: ")
+        except KeyboardInterrupt:
+            exit(0)
+
         if(pesquisa == "bairro"):
             for local in bairro:
                 print(local)
@@ -107,6 +78,9 @@ def menu(dados):
             
             escolha = input("Escolha o veiculo: ")
             search(escolha, "veiculo", veiculo)
+        
+        else:
+            print("Pesquisa incorreta.")
 
 
 def read_file(): 
