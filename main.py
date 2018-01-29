35d969821143a6aa24d922ef3794777a11e24247
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-01-29 02:44:25 -0300

    updating code

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import time
 from mining import *
 
-def search(desc, tipo="data", numero=10):
-    if(not tipo):
+def search(info, tipo, data, numero=10):
+    if(tipo == "data"):
         pass
+
+    elif(tipo == "bairro"):
+
 
     elif(tipo == "tipo"):
         pass
@@ -11,23 +15,50 @@ def search(desc, tipo="data", numero=10):
         pass
 
 
-    elif(tipo == "local"):
-        pass
+def menu(dados):
 
+    bairro, data, feridos, veiculo = get_data(dados)
 
-def menu():
     while True:
         pesquisa = input("Pesquisar por: ")
-        search(pesquisa)
+        if(pesquisa == "bairro"):
+            for local in bairro:
+                print(local)
+
+            escolha = input("Escolha o bairro: ")
+            search(escolha, "bairro", bairro)
+
+        elif(pesquisa == "data"):
+            for dia in data:
+                print(dia)
+
+            escolha = input("Escolha a data: ")
+            search(escolha, "data", data)
+
+        elif(pesquisa == "feridos"):
+            for quantidade in feridos:
+                print(quantidade)
+
+            escolha = input("Escolha a quantidade de feridos: ")
+            search(escolha, "feridos", feridos)
+
+        elif(pesquisa == "veiculo"):
+            for automovel in veiculo:
+                print(automovel)
+            
+            escolha = input("Escolha o veiculo: ")
+            search(escolha, "veiculo", veiculo)
 
 
-
-if __name__ == "__main__":
+def read_file():
     path = "data/acidentes-2016.csv"
 
     with open(path) as f:
         f.readline() # ignore first line
         dados = f.read()
+    
+    return dados
 
-    por_bairro, por_data, por_qtd_feridos, por_veiculo = get_data(dados)
-    #menu()
+if __name__ == "__main__":
+    dados = read_file()
+    menu(dados)
